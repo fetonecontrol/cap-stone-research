3d1cb7577fc8cc79aebcae2e0559a8436cb4a5be
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-26 10:19:03 -0700

    start over

--- src/reducers/firestore-reducer.js
+++
@@ -1,6 +0,0 @@
-import { firestoreReducer } from 'redux-firestore';
-import { combineReducers } from 'redux';
-
-const fireReducer = combineReducers({ firestore: firestoreReducer });
-
-export default fireReducer;
